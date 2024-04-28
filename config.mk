26ca5836c5ca1b398f306919feb53f6f2c8c5f2a
aswin7469 <aswinas@pixysos.com>
2024-04-28 18:31:28 +0530

    prebuilts: fixup soong namespace without carrierservices
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- config.mk
+++ config.mk
@@ -1,5 +1,9 @@
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/pixys-prebuilts/common
+
 # Inherit from telephony config
 $(call inherit-product, vendor/pixys-prebuilts/products/telephony.mk)
 
 # Inherit from gms config
 $(call inherit-product, vendor/pixys-prebuilts/products/gms.mk)
+
